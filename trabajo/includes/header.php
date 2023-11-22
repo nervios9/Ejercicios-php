9b64acae4671fb5a9be7b70f8bc99d7e08fa2e5e
nervios9 <sergiogomez1999@hotmail.com>
2023-11-22 11:17:50 +0100

    empezar a crear los archivos y añadir includes

---
+++ trabajo/includes/header.php
@@ -0,0 +1,8 @@
+<header>
+
+<div>
+    <img src="images/logoCPI.png" alt="No se ve">
+   <h1 class="texto">Desarrollo web en entorno Servidor</h1>
+</div>
+    
+</header> 
---
+++ trabajo/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+<?php include "includes/header.php"; ?> <!-- Aquí se incluye el contenido.php -->
+<?php include "includes/menu.php"; ?>
+<main>
+puro texto bb
+
+</main>
+<nav>
+  Menu
+  <br>
+  <a href="./index.php">Inicio</a><br> 
+  <a href="basicos/index.php">Basicos</a><br>
+  <a href="funciones/index.php">Funciones</a><br>
+  <a href="arrays/index.php">Arrays</a><br>
+  <a href="bdjardineria/index.php">BBDD</a><br>
+</nav>
+
+
+<footer>
+  <div class="redesSociales">
+    <a href="https://www.facebook.com" target="_blank"><img src="images/facebook.png" alt="Facebook"></a>
+    <a href="https://www.twitter.com" target="_blank"><img src="images/twitter.png" alt="Twitter"></a>
+    <a href="https://www.instagram.com" target="_blank"><img src="images/instagram.png" alt="Instagram"></a>
+    <a href="https://discord.gg/fTc7N6tUek" target="_blank"><img src="images/discord.png" alt="Discord"></a>
+    <a href="https://www.linkedin.com/" target="_blank"><img src="images/linkedin.png" alt="Linkedin"></a>
+  </div>
+</footer>
+</body>
+</html>
