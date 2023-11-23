3d396a63be54a1f7b54f6a31d33515e6717adf3f
nervios9 <sergiogomez1999@hotmail.com>
2023-11-23 16:38:44 +0100

    ejercicios

--- trabajo/basicos/ejer2.php
+++ trabajo/basicos/ejer2.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 <?php include "../includes/metadata2.php"; ?>
+<link rel="stylesheet" type="text/css" href="../css/estilosejer.css">
 </head>
 <body>
 <?php include "../includes/header2.php"; ?> <!-- Aquí se incluye el contenido.php -->
@@ -10,7 +11,40 @@
 
     <div class="contenedor">
     <?php include "../includes/nav_basicos.php"; ?>
-    <main>
+<main>
+<?php
+if (!$_REQUEST) {
+    ?>
+    <form action="#" method="post">
+    <p>
+      Introduce cantidad de euros a cambiar:
+      <input type="number" name="cantidad" min="0" step="0.01"><br><br>
+      Selecciona moneda destino:
+      <select name="tipo">
+        <option value="dolar">Dólar Estadounidense</option>
+        <option value="libra">Libra Esterlina</option>
+      </select>
+    </p>
+    <input type="submit" value="Enviar" name="enviar">
+  </form>
+<?php
+} else {
+    $cantidad = $_REQUEST['cantidad'];
+    $moneda   = $_REQUEST['tipo'];
+    $dolar    = 1.0563;
+    $libra    = 0.8678;
+    if ($moneda == 'dolar') {
+        $cambio = $cantidad * $dolar;
+        print "$cantidad euros equivalen a $cambio dólares estadounidenses.<br>";
+    } else {
+        $cambio = $cantidad * $libra;
+        print "$cantidad euros equivalen a $cambio libras esterlinas.<br>";
+    }
+    ?>
+    <br><a href="ejer2.php">Volver a pedir nueva conversión</a>
+    <?php     
+}
+?>
 </main>
     <?php include "../includes/aside2.php"; ?>
 </div>
--- trabajo/basicos/ejer3.php
+++ trabajo/basicos/ejer3.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 <?php include "../includes/metadata2.php"; ?>
+<link rel="stylesheet" type="text/css" href="../css/estilosejer.css">
 </head>
 <body>
 <?php include "../includes/header2.php"; ?> <!-- Aquí se incluye el contenido.php -->
@@ -10,7 +11,43 @@
 
     <div class="contenedor">
     <?php include "../includes/nav_basicos.php"; ?>
-    <main>
+<main>
+<?php
+if (!$_REQUEST) {
+    
+    ?>
+    <form action="ejer3.php" method="GET">
+				<p>Cambio de 1 euro a dólares estadounidenses: <input type="number" name="cambioDolar" step="0.0001" min="0" required></p>
+				<p>Cambio de 1 euro a libras esterlinas: <input type="number" name="cambioLibra" step="0.0001" min="0" required></p>
+				<input type="submit" value="Enviar">
+			</form>
+            <?php
+            }else{
+                $cambioDolar=$_GET['cambioDolar'];
+                $cambioLibra=$_GET['cambioLibra'];
+                $fecha=date("d-m-y");
+                echo "<h1 id='centrado'>CAMBIO DE DIVISAS A FECHA $fecha</h1>";
+                echo "<table>";
+                echo "<tr>
+                        <th>Euros</th>
+                        <th>Dolares</th>
+                        <th>Libras</th>
+                    </tr>";
+                for ($euro=1; $euro<=10 ; $euro++)
+                {
+                    if ($euro%2==0)
+                        echo "<tr class='par'>";
+                    else
+                        echo "<tr class='impar'>";
+                    echo "<td>$euro</td>";
+                    echo "<td>", $euro*$cambioDolar,"</td>";
+                    echo "<td>", $euro*$cambioLibra,"</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+
+          }
+          ?>
 </main>
     <?php include "../includes/aside2.php"; ?>
 </div>
